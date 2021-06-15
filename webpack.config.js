cb7b51993bb4d4b44589d9114352faf4e1cc494a
yakianmochi <yakianmochi.g@gmail.com>
2021-06-15 21:00:50 +0900

    htmlを監視対象にし、contentsBaseをpublic直下に変更する

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/src/',
+    filename: 'main.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    open: true,
+    watchContentBase: true,
+  },
 };
